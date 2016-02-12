5fe6adeb099320b72848f4e5b67c5982f0d97d9a
Darius Moore <darius.moore@arcdna.com>
2016-02-11 21:08:34 -0500

    Javascript File

---
+++ index.js
@@ -0,0 +1,12 @@
+/**************************************
+*       Javascripts Basics           *
+/*************************************/
+
+// Prompt command is used to "prompt" a question, through a pop-up box in the browser 
+var visitor = prompt('What is your name');
+
+// "+" signs are used to combined strings or words. This is called Concatenation.
+var message = 'Hello' + visitor + ". Welcome to Treehouse." + "We are so glad that you came to visit, " + ". Please come again, when you want to learn some more.";
+
+// Document.write prints message to the screen
+document.write(message);
